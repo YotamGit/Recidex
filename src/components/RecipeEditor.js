009f1f0335979f3e18b58b75d47a781ae3b80e4f
YotamGit <yotam.gol@gmail.com>
2021-12-08 21:34:49 +0200

    start working on recipe editor

---
+++ src/components/RecipeEditor.js
@@ -0,0 +1,20 @@
+import MarkdownEditor from "./MarkdownEditor";
+import Button from "./Button.js";
+
+const RecipeEditor = () => {
+  return (
+    <div>
+      <div>
+        <Button />
+      </div>
+      <div>
+        <Button />
+      </div>
+      <div>
+        <Button />
+      </div>
+    </div>
+  );
+};
+
+export default RecipeEditor;
