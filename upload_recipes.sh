27e9732f1651c2e5600f1658713338cc2638cee3
YotamGit <yotam.gol@gmail.com>
2022-01-07 00:42:37 +0200

    modify upload script to make back and front upload fully with one script

--- upload_recipes.sh
+++ upload_recipes.sh
@@ -13,9 +13,12 @@ scp -r build $vps_name:recipes
 ssh $vps_name sudo mv recipes /var/www/recipes
 
 # Server
+cd ../server
+mv node_modules/ ..
 cd ..
 ssh $vps_name sudo rm -rf /var/www/recipes-server recipes-server
 scp -r server $vps_name:recipes-server
+mv node_modules/ server/
 ssh $vps_name sudo mv recipes-server /var/www/recipes-server
 
 ssh $vps_name "cd /var/www/recipes-server ; npm install"
