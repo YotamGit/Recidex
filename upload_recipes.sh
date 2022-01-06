1ab0fec2f87777e4d8b5020ac547faf9cf6a1391
YotamGit <yotam.gol@gmail.com>
2022-01-06 17:15:51 +0200

    add server upload to upload file and rename server main file

--- upload_recipes.sh
+++ upload_recipes.sh
@@ -11,3 +11,9 @@ npm run build
 ssh $vps_name sudo rm -rf /var/www/recipes recipes
 scp -r build $vps_name:recipes
 ssh $vps_name sudo mv recipes /var/www/recipes
+
+# Server
+cd ..
+ssh $vps_name sudo rm -rf /var/www/recipes-server recipes-server
+scp -r server $vps_name:recipes-server
+ssh $vps_name sudo mv recipes-server /var/www/recipes-server
