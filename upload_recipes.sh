b9b31bc90f6c2f61a3085b81c9d8f8dbfd336dd8
YotamGit <yotam.gol@gmail.com>
2022-01-10 18:38:53 +0200

    add service files for backup

--- upload_recipes.sh
+++ upload_recipes.sh
@@ -5,17 +5,19 @@ set -e -x
 vps_name=${1:-oracle_vps}
 
 # Client
+sh <<EOF
 cd web-client/
 npm run build
+EOF
 
-ssh $vps_name sudo rm -rf /var/www/recipes recipes
-scp -r build $vps_name:recipes
-ssh $vps_name sudo mv recipes /var/www/recipes
+ssh $vps_name sudo rm -rf /var/www/recipes recipes-client
+scp -r web-client/build $vps_name:recipes-client
+ssh $vps_name sudo mv recipes-client /var/www/recipes
+
 
 # Server
-cd ../server
-mv node_modules/ ..
-cd ..
+mv server/node_modules/ .
+
 ssh $vps_name sudo rm -rf /var/www/recipes-server recipes-server
 scp -r server $vps_name:recipes-server
 mv node_modules/ server/
@@ -24,3 +26,9 @@ ssh $vps_name sudo mv recipes-server /var/www/recipes-server
 ssh $vps_name "cd /var/www/recipes-server ; npm install"
 ssh $vps_name sudo pm2 restart recipes-server
 
+# Services
+ssh $vps_name sudo systemctl stop recipes-backup
+scp -r system $vps_name:recipes
+ssh $vps_name sudo systemctl daemon-reload
+ssh $vps_name sudo systemctl start recipes-backup
+ssh $vps_name sudo systemctl status recipes-backup
