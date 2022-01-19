cef09dc13175cf89e876d09b5571ec4daeb85512
YotamGit <yotam.gol@gmail.com>
2022-01-19 21:37:59 +0200

    move most of the fetch new logic from recipes to main

--- web-client/src/components/Main.js
+++ web-client/src/components/Main.js
@@ -1,6 +1,36 @@
 import Recipes from "./recipes/Recipes";
+import { useEffect, useState } from "react";
 
 const Main = ({ recipes, searchFilters, getRecipes }) => {
+  const [reached_end, setReachedEnd] = useState(false);
+  const [fetching, setFetching] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = async () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        setFetching(true);
+        var res = await getRecipes({
+          latest: recipes.at(-1).creation_time,
+          count: 4,
+          filters: searchFilters,
+        });
+        setFetching(false);
+        setReachedEnd(res === 0);
+
+        if (reached_end) {
+          window.alert("No More Recipes to Show");
+        }
+      }
+
+      return;
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [recipes, reached_end, getRecipes]);
+
   return (
     <div>
       {recipes.length > 0 ? (
@@ -10,7 +40,7 @@ const Main = ({ recipes, searchFilters, getRecipes }) => {
           getRecipes={getRecipes}
         />
       ) : (
-        "No Recipes To Show"
+        "No Recipes To Show" //show skeleton loading animation if fetching is true
       )}
     </div>
   );
--- web-client/src/components/recipes/Recipes.js
+++ web-client/src/components/recipes/Recipes.js
@@ -1,38 +1,6 @@
-import { useEffect, useState } from "react";
 import RecipeCard from "./RecipeCard.js";
 import "../../styles/recipes/Recipes.css";
 const Recipes = ({ recipes, searchFilters, getRecipes }) => {
-  const [reached_end, setReachedEnd] = useState(
-    Object.keys(recipes).length === 0
-  );
-  const [fetching, setFetching] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = async () => {
-      if (
-        window.innerHeight + document.documentElement.scrollTop ===
-        document.documentElement.offsetHeight
-      ) {
-        setFetching(true);
-        var res = await getRecipes({
-          latest: recipes.at(-1).creation_time,
-          count: 4,
-          filters: searchFilters,
-        });
-        setFetching(false);
-        setReachedEnd(res === 0);
-
-        if (reached_end) {
-          window.alert("No More Recipes to Show");
-        }
-      }
-
-      return;
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [recipes, reached_end, getRecipes]);
-
   return (
     <div className="recipes-container">
       {recipes.map((recipe) => (
