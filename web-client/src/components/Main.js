8a33569386fbc5ba63165913d274047a048de938
YotamGit <yotam.gol@gmail.com>
2022-03-31 13:22:14 +0300

    add manual recipe fetching to solve a problem where there is no scrollbar on big recipes

--- web-client/src/components/Main.js
+++ web-client/src/components/Main.js
@@ -1,8 +1,13 @@
 import Recipes from "./recipes/Recipes";
 import { useEffect, useState } from "react";
 
+//redux
 import { useDispatch, useSelector } from "react-redux";
 import { getRecipes } from "../slices/recipesSlice";
+
+//mui
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Main = () => {
   const dispatch = useDispatch();
@@ -10,6 +15,23 @@ const Main = () => {
   const selectedFilters = useSelector((state) => state.filters.selectedFilters);
   const recipes = useSelector((state) => state.recipes.recipes);
   const reached_end = useSelector((state) => state.recipes.fetchedAllRecipes);
+
+  const loadRecipes = async () => {
+    if (recipes.length > 0) {
+      setFetching(true);
+      await dispatch(
+        getRecipes({
+          latest: recipes.at(-1).creation_time,
+          count: 4,
+          filters: selectedFilters,
+        })
+      );
+      setFetching(false);
+    }
+    if (reached_end) {
+      window.alert("No More Recipes to Show");
+    }
+  };
 
   useEffect(() => {
     const handleScroll = async () => {
@@ -20,19 +42,8 @@ const Main = () => {
         // window.alert(
         //   `${window.innerHeight} + ${document.documentElement.scrollTop} = ${document.documentElement.offsetHeight}`
         // );
-        setFetching(true);
-        await dispatch(
-          getRecipes({
-            latest: recipes.at(-1).creation_time,
-            count: 4,
-            filters: selectedFilters,
-          })
-        );
-        setFetching(false);
 
-        if (reached_end) {
-          window.alert("No More Recipes to Show");
-        }
+        await loadRecipes();
       }
 
       return;
@@ -42,11 +53,29 @@ const Main = () => {
   }, [recipes, reached_end, getRecipes]);
 
   return (
-    <div>
+    <div
+      className="main"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
       {recipes.length > 0 ? (
         <Recipes />
       ) : (
         "No Recipes To Show" //show skeleton loading animation if fetching is true
+      )}
+      {!reached_end && (
+        <div>
+          {fetching ? (
+            <CircularProgress />
+          ) : (
+            <Button variant="contained" component="div" onClick={loadRecipes}>
+              Load Recipes
+            </Button>
+          )}
+        </div>
       )}
     </div>
   );
