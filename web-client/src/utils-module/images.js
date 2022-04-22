36a8222a83cd4bdcf7bb169756addf3ef6badf30
YotamGit <yotam.gol@gmail.com>
2022-04-22 10:36:40 +0300

    add print css

--- web-client/src/utils-module/images.js
+++ web-client/src/utils-module/images.js
@@ -6,3 +6,30 @@ exports.toBase64 = (file) =>
     reader.onload = () => resolve(reader.result);
     reader.onerror = (error) => reject(error);
   });
+/*function resizeImage(base64Str) {
+
+      var img = new Image();
+      img.src = base64Str;
+      var canvas = document.createElement('canvas');
+      var MAX_WIDTH = 400;
+      var MAX_HEIGHT = 350;
+      var width = img.width;
+      var height = img.height;
+
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
+      }
+      canvas.width = width;
+      canvas.height = height;
+      var ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, width, height);
+      return canvas.toDataURL();
+    } */
