40bdff02fe8bad535ebe945fd00dc982796a7cd4
YotamGit <yotam.gol@gmail.com>
2022-04-25 13:06:58 +0300

    rewrite migrate, successfully migrate db to webp

--- migrations/base64toWebp.js
+++ migrations/base64toWebp.js
@@ -3,7 +3,8 @@ import sharp from "sharp";
 
 async function reduceImgQuality(base64Image, id, title) {
   try {
-    let parts = base64Image.split(";");
+    console.log(title);
+    var parts = base64Image.split(";");
     let mimType = parts[0].split(":")[1];
     let imageData = parts[1].split(",")[1];
     var img = new Buffer.from(imageData, "base64");
@@ -17,41 +18,63 @@ async function reduceImgQuality(base64Image, id, title) {
       id,
       title
     );
+    console.log(resizedImageBuffer);
     return resizedImageBuffer;
   } catch (err) {
+    // console.log(err);
+    console.log(parts);
     console.log("cant convert image, type: ", typeof base64Image, id, title);
+    return null;
   }
 }
 
 // Connection URI
 const uri = "mongodb://localhost:27017/Recipes";
-// Create a new MongoClient
 
-async function run() {
-  const client = await MongoClient.connect(uri);
+async function getRecipes() {
+  const client = new MongoClient(uri);
   try {
     // Connect the client to the server
+    await client.connect();
     // Establish and verify connection
     const collection = await client.db("Recipes").collection("recipes");
 
-    await collection.find({}).forEach(
-      async (recipe) =>
-        await collection.updateOne(
-          { _id: recipe._id },
-          {
-            $set: {
-              image: await reduceImgQuality(
-                recipe.image,
-                recipe._id,
-                recipe.title
-              ),
-            },
-          }
-        )
+    var recipes = await collection.find({}).toArray();
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+  return recipes;
+}
+
+async function convertRecipe(recipe) {
+  const client = new MongoClient(uri);
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    const collection = await client.db("Recipes").collection("recipes");
+
+    const res = await collection.updateOne(
+      { _id: recipe._id },
+      {
+        $set: {
+          image: await reduceImgQuality(recipe.image, recipe._id, recipe.title),
+        },
+      }
     );
+    console.log(res);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);
+
+try {
+  var recipes = await getRecipes();
+  console.log(recipes.length);
+} catch (err) {
+  console.dir(err);
+}
+
+recipes.map((recipe) => convertRecipe(recipe));
