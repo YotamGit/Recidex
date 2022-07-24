3cdc8a6b1a9231b683f62c9111e9f5309a601bae
YotamGit <yotam.gol@gmail.com>
2022-07-24 18:01:50 +0300

    continue working on upload script

--- upload_recipes.sh
+++ upload_recipes.sh
@@ -4,31 +4,27 @@ set -e -x
 
 vps_name=${1:-oracle_vps}
 
-# Client
-sh <<EOF
-cd web-client/
-npm run build
-EOF
+# Build images
+docker-compose -f docker-compose.prod.yml build
 
-ssh $vps_name sudo rm -rf /var/www/recidex recidex-client
-scp -r web-client/build $vps_name:recidex-client
-ssh $vps_name sudo mv recidex-client /var/www/recidex
+# Copy compose file to server
+ssh $vps_name 'mkdir -p recidex'
+scp docker-compose.prod.yml $vps_name:recidex/docker-compose.yml
+
+# Push and pull images
+ssh $vps_name 'sudo docker container start docker-registry'
+docker-compose -f docker-compose.prod.yml push
+ssh $vps_name 'cd ~/recidex && sudo docker-compose pull'
+ssh $vps_name 'sudo docker container stop docker-registry'
+
+# ssh $vps_name sudo rm -rf /var/www/recidex recidex-client
+# scp -r web-client/build $vps_name:recidex-client
+# ssh $vps_name sudo mv recidex-client /var/www/recidex
 
 
-# Server
-mv server/node_modules/ .
-
-ssh $vps_name sudo rm -rf /var/www/recipes-server recipes-server
-scp -r server $vps_name:recipes-server
-mv node_modules/ server/
-ssh $vps_name sudo mv recipes-server /var/www/recipes-server
-
-ssh $vps_name "cd /var/www/recipes-server ; npm install"
-ssh $vps_name pm2 restart recipes-server
-
-# Services
-ssh $vps_name sudo systemctl stop recipes-backup
-scp -r system $vps_name:recipes
-ssh $vps_name sudo systemctl daemon-reload
-ssh $vps_name sudo systemctl start recipes-backup
-ssh $vps_name sudo systemctl status recipes-backup
+#Services
+# ssh $vps_name sudo systemctl stop recipes-backup
+scp -r system $vps_name:recidex
+# ssh $vps_name sudo systemctl daemon-reload
+# ssh $vps_name sudo systemctl start recipes-backup
+# ssh $vps_name sudo systemctl status recipes-backup
